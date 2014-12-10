d3279e4c34cded33eeea78519aef3e91fd40689f
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-12-10 17:28:59 -0600

    Extract Method: PasswordGenerator::defaultCharacters.

--- src/PasswordGenerator.php
+++ src/PasswordGenerator.php
@@ -23,7 +23,7 @@ class PasswordGenerator
      */
     public function __construct($characters = null, $length = 32)
     {
-        $this->_characters = $characters ?: join(range(chr(32), chr(126)));
+        $this->_characters = $characters ?: self::defaultCharacters();
         $this->_length = $length;
     }
 
@@ -37,5 +37,17 @@ class PasswordGenerator
     {
         $generator = (new RandomFactory())->getMediumStrengthGenerator();
         return $generator->generateString($this->_length, $this->_characters);
+    }
+
+    /**
+     * Returns the default characters used by the generator.
+     *
+     * This is set to all of the printable ASCII characters.
+     *
+     * @return string The default characters used.
+     */
+    public static function defaultCharacters()
+    {
+        return join(range(chr(32), chr(126)));
     }
 }
